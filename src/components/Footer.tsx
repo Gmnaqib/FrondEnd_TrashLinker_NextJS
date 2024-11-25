2239acbdb3a89145ecff469cb4aac7bf87c58015
dewatriwijaya <dewatwc@gmail>
2024-11-25 16:02:52 +0700

    Add Footer In Home And Add Folder Fitur

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,31 +2,44 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center px-4 lg:px-40 py-4 h-12 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center space-y-3 sm:mb-0 mb-3 border-gray-200">
-      <div className="flex flex-col">
-        <a className="flex text-teal-600 items-center" href="#">
-          <img className="h-16" src="/logo.png" />
+    <footer className="bg-slate-300 text-center border-t mt-5 flex sm:flex-row flex-col justify-between items-center space-y-3 sm:mb-0 mb-3 p-6 border-gray-200">
+      <div className="flex flex-col sm:flex-row sm:justify-between w-full items-center">
+        {/* Logo dan Nama */}
+        <a className="flex text-teal-600 items-center space-x-2">
+          <img className="h-16" src="/logo.png" alt="TrashLinker Logo" />
           <div>
             <span className="text-black text-2xl font-bold">Trash</span>
             <span className="text-[rgba(50,195,108,1)] text-2xl font-bold">Linker</span>
           </div>
         </a>
 
-        <div className="flex flex-row">
-        <h2>Jl. Dipati ukur No. 176
-          Kec. Sukahaji, Kota Bandung
-          Jawa Barat 40221</h2>
-        <div>
-          <span>0867-5678-3456 (WA CS 1)</span>
-          <span>0867-5678-3456 (WA CS 2)</span>
-          <span>info.trashlinker@gmail.com</span>
-        </div>
+        {/* Alamat, Kontak, dan Partner */}
+        <div className="flex sm:flex-row flex-col sm:space-x-10 space-y-5 sm:space-y-0 mt-5 sm:mt-0">
+          {/* Alamat */}
+          <div className="flex flex-col space-y-2">
+            <span>Jl. Dipati Ukur No. 176</span>
+            <span>Kec. Sukahaji, Kota Bandung</span>
+            <span>Jawa Barat 40221</span>
+          </div>
 
-        <h2>
-        Partner Kami
-        </h2>
+          {/* Kontak */}
+          <div className="flex flex-col space-y-2">
+            <span>0867-5678-3456 (WA CS 1)</span>
+            <span>0867-5678-3456 (WA CS 2)</span>
+            <span>info.trashlinker@gmail.com</span>
+          </div>
+
+          {/* Partner */}
+          <div className="flex flex-col space-y-2 items-center sm:items-start">
+            <h2 className="font-semibold">Partner Kami</h2>
+            <div className="flex space-x-4">
+              <img src="/img/img_bank.png" className="w-20" alt="Bank Partner" />
+              <img src="/img/img_sipsn.png" className="w-20" alt="SIPSN Partner" />
+            </div>
+          </div>
         </div>
       </div>
     </footer>
+
   );
 }
